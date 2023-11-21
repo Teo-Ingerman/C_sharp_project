dbd762bdba1f0acbc3a7439b5cf6b0d1cb755bb5
Teo Ingerman <teo.ingerman@elev.ga.ntig.se>
2023-11-21 18:57:05 +0100

    yes

---
+++ app/Matte.cs
@@ -0,0 +1,12 @@
+
+namespace Matte
+{
+    public class Matte
+    {
+        public int Add_numbers(int num_1, int num_2) 
+        {
+            Console.WriteLine("no");
+            return (num_1 + num_2);
+        }
+    }
+}
--- app/Program.cs
+++ app/Program.cs
@@ -1,44 +1,47 @@
-﻿using System.Diagnostics;
-// See https://aka.ms/new-console-template for more information
-using System;
-using Enemies;
-using System.Drawing.Printing;
+﻿using System;
+using System.Collections;
 
-
-namespace Game
+namespace mineswwep
 {
+  class Brain
+  {
+    
+    public SortedList fslist = new();
+
+    public Brain(string first_color)
+    {
+
+
+      Console.WriteLine("ne");
+    }
+      
+  }
+
+    
+
+
   class Program
   {
+
+
+
     static void Main(string[] args)
+
     {
       
-      Console.WriteLine("What's your first name?");
-      string first_name = Console.ReadLine();
+
+      Brain minesweeper_brain = new("*hexcode*");
+
+
+
+
+
+
+    
       
-      Console.WriteLine("What's your last name?");
-      string last_name = Console.ReadLine();
-
-      Thread.Sleep(200);
-
-      Console.WriteLine($"hello {first_name} {last_name}, you suck");
-
-
-      Console.WriteLine("product price: ");
-      float price = Convert.ToSingle(Int32.Parse(Console.ReadLine()));
-
-      Console.WriteLine("amount: ");
-      int amount = Int32.Parse(Console.ReadLine());
-
-      float cost = amount * price;
-
-      Console.WriteLine(price);
-
-
-
-
-
-
-
+      
+      
     }
   }
+
 }

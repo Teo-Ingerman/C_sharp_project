f76f110b4cc8d5e87504f2a731674a8c071f5392
Teo Ingerman <teo.ingerman@elev.ga.ntig.com>
2023-11-21 20:41:16 +0100

    yes

--- app/Program.cs
+++ app/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System;
+using System.Windows;
+
 
 namespace mineswwep
 {
+
   class Brain
   {
     
@@ -14,8 +19,9 @@ namespace mineswwep
 
       Console.WriteLine("ne");
     }
-      
+    
   }
+
 
     
 
@@ -23,13 +29,35 @@ namespace mineswwep
   class Program
   {
 
-
-
+    
+    // public SupportedOSPlatformAttribute (string platformName);
+  
     static void Main(string[] args)
 
     {
       
 
+
+      static Bitmap CaptureScreen()
+    {
+        // Create a bitmap object to hold the screenshot
+        Bitmap screenshot = new(1920, 1080);
+
+        // Create a graphics object from the bitmap
+        using (Graphics g = Graphics.FromImage(screenshot))
+        {
+            // Copy the screen to the bitmap
+            g.CopyFromScreen(0, 0, 0, 0, screenshot.Size);
+        }
+
+        return screenshot;
+    }
+
+    static Color GetPixelColor(Bitmap screenshot, int x, int y)
+    {
+        // Get the color of the specified pixel
+        return screenshot.GetPixel(x, y);
+    }
       Brain minesweeper_brain = new("*hexcode*");
 
 
